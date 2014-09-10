1f8a17a86734d7f7d26a0ee4f65956a93d4a8995
arazin <hoge@hoge.hoge>
2014-09-10 14:27:44 +0900

    ゲームロジックを実装するクラスを追加

---
+++ FuyomiDrillCore/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuyomiDrillCore
+{
+    public class FuyomiGame
+    {
+        private int level;
+        private List<List<string>> qSet;
+
+        public FuyomiGame()
+        {
+
+        }
+
+        public void GameStart()
+        {
+
+        }
+
+        public string NextQuestion()
+        {
+            return "end";
+        }
+
+        public bool IsCorrect(string ans)
+        {
+            return false;
+        }
+
+        public string Result()
+        {
+            return "count";
+        }
+    }
+}

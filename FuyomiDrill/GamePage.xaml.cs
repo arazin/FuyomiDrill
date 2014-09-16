1404dfa44ede681a5f96032aad4e2910b9e0913e
arazin <hoge@hoge.hoge>
2014-09-16 16:58:17 +0900

    正否アニメーションの追加

--- FuyomiDrill/GamePage.xaml.cs
+++ FuyomiDrill/GamePage.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using FuyomiDrillCore;
 using System.IO;
+using System.Threading;
+using System.Windows.Media.Animation;
 
 namespace FuyomiDrill
 {
@@ -26,35 +28,70 @@ namespace FuyomiDrill
         private int level;
         private FuyomiGame game;
         private MainWindow mainWindow;
+        private BitmapImage correctImage;
+        private BitmapImage falseImage;
+        private BitmapImage clearImage;
+        private List<Button> keyList;
 
         public GamePage()
         {
             InitializeComponent();
 
+            keyList = new List<Button>() { C, Cs, D, Ds, E, F, Fs, G, Gs, A, As, B };
+
             mainWindow = App.GetWindow("NavWin") as MainWindow;
+
+            correctImage = new BitmapImage(new Uri(@"..\..\Correct.png",UriKind.Relative));
+            falseImage = new BitmapImage(new Uri(@"..\..\False.png", UriKind.Relative));
+            clearImage = new BitmapImage(new Uri(@"..\..\Clear.png",UriKind.Relative));
 
             game = new FuyomiGame(((int)Application.Current.Properties["level"]) + 1);
             this.qTextBox.Text = game.GameStart();
             level = (int)Application.Current.Properties["level"];
         }
 
+        //private void invalidateButton()
+        //{
+        //    foreach (Button key in keyList)
+        //    {
+        //        key.IsEnabled = false;
+        //    }
+        //}
+
+        //private void validateButton()
+        //{
+        //    foreach (Button key in keyList)
+        //    {
+        //        key.IsEnabled = true;
+        //    }
+        //}
+
+
         private void keyClick(object sender, RoutedEventArgs e)
         {
-            Button keyButton = null;
             string ans = "";
             string nextQuestion = "";
-
+            Button keyButton = null;
             if (sender is Button)
             {
                 keyButton = (Button)sender;
                 ans = keyButton.Name;
             }
 
-            nextQuestion = game.IsCorrect(ans);
+            DiscreteObjectKeyFrame setImageFrame = new DiscreteObjectKeyFrame();
+            DiscreteObjectKeyFrame clearImageFrame = new DiscreteObjectKeyFrame();
+            DiscreteObjectKeyFrame setQuestionFrame = new DiscreteObjectKeyFrame();
+            setImageFrame.KeyTime = new TimeSpan(0, 0, 0, 0);
+            clearImageFrame.KeyTime = new TimeSpan(0, 0, 0, 0, 700);
+            clearImageFrame.Value = clearImage;
+            setQuestionFrame.KeyTime = new TimeSpan(0, 0, 0, 0, 700);
 
+            nextQuestion = game.IsCorrect(ans);
             if (nextQuestion == "false")
             {
+                setImageFrame.Value = falseImage;
                 mainWindow.setStatus("False.");
+                setQuestionFrame.Value = qTextBox.Text;
             }
             else if (nextQuestion == "end")
             {
@@ -65,10 +102,31 @@ namespace FuyomiDrill
             }
             else
             {
+                setImageFrame.Value = correctImage;
+                //rightOrWrongImage.Source = correctImage;
                 mainWindow.setStatus("Correct.");
                 this.qTextBox.Text = nextQuestion;
+                //setQuestionFrame.Value = nextQuestion;
             }
+            ObjectAnimationUsingKeyFrames rightOrWrongAnimation = new ObjectAnimationUsingKeyFrames();
+            rightOrWrongAnimation.BeginTime = new TimeSpan(0,0,0,0);
+            rightOrWrongAnimation.KeyFrames.Add(setImageFrame);
+            rightOrWrongAnimation.KeyFrames.Add(clearImageFrame);
 
+            ObjectAnimationUsingKeyFrames questionAnimation = new ObjectAnimationUsingKeyFrames();
+            questionAnimation.BeginTime = new TimeSpan(0, 0, 0, 0);
+            questionAnimation.KeyFrames.Add(setQuestionFrame);
+
+            Storyboard gameStoryboard = new Storyboard();
+            gameStoryboard.Children.Add(rightOrWrongAnimation);
+            Storyboard.SetTargetName(rightOrWrongAnimation, rightOrWrongImage.Name);
+            Storyboard.SetTargetProperty(rightOrWrongAnimation, new PropertyPath(Image.SourceProperty));
+
+            //gameStoryboard.Children.Add(questionAnimation);
+            //Storyboard.SetTargetName(questionAnimation, qTextBox.Name);
+            //Storyboard.SetTargetProperty(questionAnimation,new PropertyPath(TextBlock.TextProperty));
+
+            gameStoryboard.Begin(this);
         }
 
 
